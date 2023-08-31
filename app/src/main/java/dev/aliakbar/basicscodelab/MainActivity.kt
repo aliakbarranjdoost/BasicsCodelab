42b1ae536388fb9ecd91fdab1a922e3c98785668
AliAkbar <aliakbarranjdoost@gmail.com>
2023-08-31 12:08:16 +0330

    Pathway 1 - Activity 4 - Part 10 : Persisting state

--- app/src/main/java/dev/aliakbar/basicscodelab/MainActivity.kt
+++ app/src/main/java/dev/aliakbar/basicscodelab/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,7 +46,7 @@ class MainActivity : ComponentActivity()
 @Composable
 fun MyApp(modifier: Modifier = Modifier)
 {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier = modifier)
     {
